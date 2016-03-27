7ec57ecbb926d12d33d8d710f0a791bd35a17e76
Nicolas Truyens <nicolas@truyens.com>
2016-03-27 12:17:08 +0200

    Puppet 4 fixes

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -15,18 +15,15 @@
 # Example42 dependencies (sigh)
 #
 class bareos::repository inherits bareos {
-
-  if ($bool_manage_repository){
+  if ($bool_manage_repository) {
       case $::operatingsystem {
-
-        redhat,centos,fedora,Scientific,OracleLinux: {
+        'Redhat','CentOS','Fedora','Scientific','OracleLinux': {
           file { 'bareos.repo':
             path    => '/etc/yum.repos.d/bareos.repo',
             content => template('bareos/bareos.repo.erb'),
           }
         }
-
-        Debian,Ubuntu: {
+        'Debian','Ubuntu': {
           file { '/etc/apt/sources.list.d/bareos.list':
             content => "deb http://download.bareos.org/bareos/release/${bareos::repo_flavour}/${bareos::repo_distro} /\n"
           }
